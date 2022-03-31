590dbe9ecde0337eb3e7b8d623afe50297423d82
dhokoon <52174999+dhokoon@users.noreply.github.com>
2022-03-31 04:07:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+total_people = len(names)
+
+random_person = random.randint(0, total_people - 1)
+
+choice_payer = names[random_person]
+
+print(choice_payer)
+
